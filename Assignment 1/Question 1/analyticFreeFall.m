b70d4f84f8777047ccbfd8bb65b5560b7e1a86c3
Bhupinder Singh <bhupi98.bs@gmail.com>
2024-01-13 17:17:40 -0800

    Question 1 Assignment 1 complete

---
+++ Assignment 1/Question 1/analyticFreeFall.m
@@ -0,0 +1,17 @@
+function analyticFreeFall(m,c,g,t0,v0,tn,n)
+% analytic solution to find free falling vellocity 
+%   print headings and initial conditions
+fprintf('values of t    approximations v(t)\n')
+fprintf('%8.3f',t0),fprintf('%19.4f\n',v0)
+%   compute step size h
+h=(tn-t0)/n;
+
+t=0;
+% compute velocity
+for i=1:n 
+    v=(g*m/c)*(1-exp(-c*t/m));
+    t=t+h;
+    fprintf('%8.3f',t), fprintf('%19.4f\n',v)
+end
+end
+
