2ef47b935e51e34b3700c6f8561b1e2b798ca9f1
Bhupinder Singh <bhupi98.bs@gmail.com>
2024-01-13 19:58:30 -0800

    done

--- Assignment 1/Question 3/seriesRelativeError.m
+++ Assignment 1/Question 3/seriesRelativeError.m
@@ -2,15 +2,22 @@ function seriesRelativeError(j)
 p=2.718281;
 n=0;
 error=Inf;
-while(error>(5*10^-n))
-    n=n+1;
+sigFigs=4;
+while(error>(5*10^-sigFigs))
+    sSf=sigFigs; %sig figs of the approximation
     sum = 0;
     %   approximation
     for l=0:n
         sum = sum + 1/factorial(l);
     end
-    sum
-    error = abs(1-sum/p)
+    error = abs(1-sum/p);
+    while(error>(5*10^-sSf) && sSf>=0)
+        sSf= sSf-1;
+    end
+    fprintf("Approximation has %d sig Figs and n=%d\n", sSf,n);
+    fprintf("%2.6f\n",sum);
+    fprintf("%2.6f\n\n",error);
+    n=n+1;
 end
 end
 
